76a8933a6c5aa5109e3b55284c2e06934ba05053
dadum <sir.dadum@gmail.com>
2020-01-01 04:42:00 +0100

    add add map configuration

--- Config.lua
+++ Config.lua
@@ -9,6 +9,7 @@ local GetTabs = function()
     return tabs
 end
 
+-- get map as string
 local GetMapString = function(info)
     local str = ""
     for k, v in pairs(core.conf.map) do
@@ -19,15 +20,14 @@ local GetMapString = function(info)
     return str
 end
 
-local ParseMap = function(info, val)
+-- parse input string to map
+local ParseMapString = function(info, val)
     local linesplit = {strsplit("\n", val)}
     local temp = {}
     for _, i in pairs(linesplit) do
         local key, tab = strsplit("-", i)
         -- skip empty lines
         if key and tab then
-            print(key)
-            print(tab)
             local tabnum = tonumber(tab)
             if not tabnum then
                 print(
@@ -43,6 +43,18 @@ local ParseMap = function(info, val)
         end
     end
     core.conf.map = temp
+end
+
+local AddMap = function()
+    if core.t1 and core.t2 then
+        local id = GetItemInfoInstant(core.t1)
+        if not id then print(L["ERROR: invalid item."]) 
+        return
+        end
+        table.insert(core.conf.map, id, core.t2)
+    else 
+        print("ERROR: invalid input.")
+    end
 end
 
 local generalOptions = {
@@ -90,72 +102,106 @@ local generalOptions = {
         step = 0.1,
         set = function(info, val) core.conf.withdrawInterval = val end,
         get = function(info) return core.conf.withdrawInterval end
-    },
-    slashtitle = {name = L["Slash commands"], type = 'header', order = 20},
-    gdesc = {
-        name = L["/gd | /guilddeposit: open GuildDeposit configuration."],
-        type = 'description',
-        order = 30
-    },
-    depdesc = {
-        name = L["/gdeposit | /gdep: deposit items."],
-        type = 'description',
-        order = 40
-    },
-    withdesc = {
-        name = L["/gdwithdraw <tab_number>: withdraw all the items from tab <tab_number>. If no number is specified, the current tab will be used."],
-        type = 'description',
-        order = 50
-    },
-    mapbagdesc = {
-        name = L["/gdbag <bag_nubmer> <tab_number>: add all the items contained in bag <bag_number> to the map for tab <tab_number>."],
-        type = 'description',
-        order = 60
-    },
-    maptabdesc = {
-        name = L["/gdtab <tab_number>: add all the items in tab <tab_number>. If no tab is provided, the current tab is used."],
-        type = 'description',
-        order = 70
-    },
-    printmapdesc = {
-        name = L["/gdprint: prints the mappings in chat"],
-        type = 'description',
-        order = 80
-    },
-    clearmapdesc = {
-        name = L["/gdclear: clears all mappings"],
-        type = 'description',
-        order = 90
     }
 }
 
 local mapOptions = {
-    testText = {
+    addSection = {name = L["Add Map"], type = 'header', order = 0},
+    addInput = {
         name = L["Map Item"],
-        desc = L["Add one item to the mappings"],
+        desc = L["Add one item to the mappings. See usage for format."],
         type = 'input',
+        order = 10,
         set = function(info, val) core.t1 = val end,
         get = function(info) return core.t1 end
     },
-    testSelect = {
+    tabSelect = {
         name = L["Select Tab"],
         type = 'select',
+        width = 0.8,
+        order = 20,
         values = GetTabs,
         set = function(info, val) core.t2 = val end,
         get = function(info) return core.t2 end
     },
-    testButton = {
+    addButton = {
         name = L["Add"],
         type = 'execute',
-        func = function() print(core.t2 .. core.t1) end
+        order = 30,
+        width = 0.5,
+        func = AddMap
     },
-    testMultisel = {
-        name = 'test',
+    mapSection = {name = L["Map"], type = 'header', order = 40},
+    mapInput = {
+        name = L["Map"],
+        desc = L["Full representation of the saved map. See usage for possible modifications."],
         type = 'input',
+        order = 50,
         multiline = 10,
         width = 'full',
+        -- get map as string
         get = GetMapString,
-        set = ParseMap
+        -- parse input string to map
+        set = ParseMapString
+    }
+}
+
+local usageOptions = {
+    addTitle = {
+        name = L["Add Map"],
+        type = 'header',
+        order = 10
+    },
+    addDesc = {
+        name = L["Item name: item name (must be in bag) | item link | item id"],
+        type = 'description',
+        order = 20
+    },
+    mapAddDesc = {
+        name = L["Add mapping format: <item>-<tab>. <item>: item name (must be in bags) | item link | item id. <tab>: tab number between 1 and 6 (tab must be owned)."],
+        type  = 'description',
+        order = 30
+    },
+    mapDeleteDesc = {
+        name = L["Just delete the desired row and accept."],
+        type = 'description',
+        order = 40
+    },
+    slashTitle = {name = L["Slash commands"], type = 'header', order = 50},
+    gdesc = {
+        name = L["/gd | /guilddeposit: open GuildDeposit configuration."],
+        type = 'description',
+        order = 60
+    },
+    depdesc = {
+        name = L["/gdeposit | /gdep: deposit items."],
+        type = 'description',
+        order = 70
+    },
+    withdesc = {
+        name = L["/gdwithdraw <tab_number>: withdraw all the items from tab <tab_number>. If no number is specified, the current tab will be used."],
+        type = 'description',
+        order = 80
+    },
+    mapbagdesc = {
+        name = L["/gdbag <bag_nubmer> <tab_number>: add all the items contained in bag <bag_number> to the map for tab <tab_number>."],
+        type = 'description',
+        order = 90
+    },
+    maptabdesc = {
+        name = L["/gdtab <tab_number>: add all the items in tab <tab_number>. If no tab is provided, the current tab is used."],
+        type = 'description',
+        order = 100
+    },
+    printmapdesc = {
+        name = L["/gdprint: prints the mappings in chat"],
+        type = 'description',
+        order = 110
+    },
+    clearmapdesc = {
+        name = L["/gdclear: clears all mappings"],
+        type = 'description',
+        order = 120
     }
 }
 
@@ -173,6 +219,12 @@ core.options = {
             type = 'group',
             order = 20,
             args = mapOptions
+        },
+        usageOpt = {
+            name = L["Usage"],
+            type = 'group',
+            order = 30,
+            args = usageOptions
         }
     }
 }
